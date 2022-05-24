d8a2cc75d610026001e9ab79d4f36e05ead61aa1
k3nryu <tinkenryu@gmail.com>
2022-05-24 21:14:38 +0900

    add npm installation script

---
+++ Install_JavaScripts_via_npm.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+npm install -g hexo-cli
+npm install hexo-deployer-git --save
+npm install hexo-wordcount --save
+npm install hexo-generator-json-content --save
+npm install hexo-generator-feed --save
+npm install hexo-generator-sitemap --save
+#npm install hexo-generator-baidu-sitemap --save
