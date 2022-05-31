c694faea41be819290ec36f3b44ecfb678790a0f
k3nryu <tinkenryu@gmail.com>
2022-06-01 00:29:27 +0900

    fix bug

--- joplin2hexo/joplin_api.sh
+++ joplin2hexo/joplin_api.sh
@@ -1,13 +1,48 @@
 #! /bin/bash
 
-joplin_srv_ip=localhost
-joplin_srv_port=41184
-note_id=fa648cf2a039497da5eee0b61d454892
-joplin_token=b03f620aac099e9c9ca929eccc95f361ea6de8db4275ddde31214dfef346d8adefc95ff25bccec8633d291fd558c4fade067ee6b807eb389f38d284ed06dc9e5
+
+# Get hexo server directory.
+hexo_dir=`..`
+mkdir -p $hexo_dir/tmp $hexo_dir/source/resources
+
+# hexo post resources directory.
+local_rsc_dir=$hexo_dir/source/resources/
+
+if [[ -e "./joplin_user_profile.sh" ]];then
+        source ./joplin_user_profile.sh
+        echo -e "User profile successfully obtained!"
+else
+        echo -e "User profile[./joplin_user_profile.sh] not found,\nPlease enter your Joplin server user name:"
+        read joplin_srv_user
+        echo -e "Please enter your Joplin server IP:"
+        read joplin_srv_ip
+        echo -e "Please enter your Joplin server token:"
+        read joplin_srv_token
+        echo -e "Please enter your Joplin server OS(mac or win):"
+        read joplin_os
+        if [[ $joplin_os == win ]];then
+                joplin_rsc_dir=$joplin_srv_user@$joplin_srv_ip:/C:/Users/$joplin_srv_user/.config/joplin-desktop/resources/
+        elif [[ $joplin_os == mac ]];then
+                joplin_rsc_dir=$joplin_srv_user@$joplin_srv_ip:/Users/$joplin_srv_user/.config/joplin-desktop/resources/
+        else
+                echo - "Input error!"
+        fi
+
+fi
+
+echo -e 'The following parameters were read:\n'
+echo '<<<EOF'
+echo hexo_dir=`pwd`
+echo joplin_srv_user=$joplin_srv_user
+echo joplin_srv_ip=$joplin_srv_ip
+echo joplin_srv_token=$joplin_srv_token
+echo joplin_rsc_dir=$joplin_rsc_dir
+echo -e 'EOF\n'
+
 
 # --- Useful ---
 # Get note body(json format) by note id.
-curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id?token=$joplin_token\&fields=body
+curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id?token=$joplin_srv_token\&fields=body
 
 
 # --- tested  ---
@@ -15,33 +50,33 @@ curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id?token=$joplin_token\&
 #curl http://$joplin_srv_ip:$joplin_srv_port/ping
 
 # Testing the token whether avalible
-#curl http://$joplin_srv_ip:$joplin_srv_port/auth/check/\?token\=$joplin_token
+#curl http://$joplin_srv_ip:$joplin_srv_port/auth/check/\?token\=$joplin_srv_token
 
 # Gets all notes
-#curl http://$joplin_srv_ip:$joplin_srv_port/notes/\?token\=$joplin_token
+#curl http://$joplin_srv_ip:$joplin_srv_port/notes/\?token\=$joplin_srv_token
 
 # Gets note with ID :id
-#curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id/\?token\=$joplin_token
+#curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id/\?token\=$joplin_srv_token
 
 # Gets all the tags attached to this note.
-#curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id/tags/\?token\=$joplin_token
+#curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id/tags/\?token\=$joplin_srv_token
 
 # Gets all the resources attached to this note.
-#curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id/resources/\?token\=$joplin_token
+#curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id/resources/\?token\=$joplin_srv_token
 
 # Gets the IDs only of all the tags:
-#curl http://$joplin_srv_ip:$joplin_srv_port/tags/?fields=id\&token=$joplin_token
+#curl http://$joplin_srv_ip:$joplin_srv_port/tags/?fields=id\&token=$joplin_srv_token
 
 # Get token 
 #curl -XPOST http://$joplin_srv_ip:$joplin_srv_port/auth
 #curl  http://$joplin_srv_ip:$joplin_srv_port/auth/check?auth_token=AUTH_TOKEN
 
 # Get the note's location by note id
-#curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id?fields=longitude,latitude\&token=$joplin_token
+#curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id?fields=longitude,latitude\&token=$joplin_srv_token
 
-#echo http://$joplin_srv_ip:$joplin_srv_port/tags?token=$joplin_token
+#echo http://$joplin_srv_ip:$joplin_srv_port/tags?token=$joplin_srv_token
 
-#curl http://$joplin_srv_ip:$joplin_srv_port/notes/"$note_id?fields"=longitude,latitude?"token"=$joplin_token
-#curl http://$joplin_srv_ip:$joplin_srv_port/tags?fields=id&token=$joplin_token
-#curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id?"token"=$joplin_token
+#curl http://$joplin_srv_ip:$joplin_srv_port/notes/"$note_id?fields"=longitude,latitude?"token"=$joplin_srv_token
+#curl http://$joplin_srv_ip:$joplin_srv_port/tags?fields=id&token=$joplin_srv_token
+#curl http://$joplin_srv_ip:$joplin_srv_port/notes/$note_id?"token"=$joplin_srv_token
 
