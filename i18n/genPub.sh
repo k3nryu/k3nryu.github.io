41d4156de8e87995ec686bcee3f31284de33890d
k3nryu <tinkenryu@gmail.com>
2022-06-26 11:07:14 +0900

    update i18n script

---
+++ i18n/genPub.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# Get hexo server directory.
+hexo_dir=`realpath $(dirname $0) | sed -e 's/\/[0-9a-zA-Z]*$//'`
+
+cd $hexo_dir/
+
+# Clean 'public' folder
+hexo clean 
+
+# Generate i18n 'public' folder
+for lang in en zh ja
+do
+#ln -sf i18n/hexo_config_$lang.yml _config.yml
+#ln -sf ../../i18n/pure_config_$lang.yml themes/pure/_config.yml
+	ln -sf $hexo_dir/i18n/hexo_config_en.yml $hexo_dir/_config.yml
+	ln -sf $hexo_dir/i18n/pure_config_en.yml $hexo_dir/themes/pure/_config.yml
+	hexo g 
+	if [ $lang == en ]
+	then
+		mv $hexo_dir/public $hexo_dir/public_en
+	else
+		mv $hexo_dir/public $hexo_dir/public_en/$lang
+	fi
+done
+mv public_en public
+
+# Reset config file for 'hexo s'
+ln -sf $hexo_dir/i18n/hexo_config_en.yml $hexo_dir/_config.yml
+ln -sf $hexo_dir/i18n/pure_config_en.yml $hexo_dir/themes/pure/_config.yml
